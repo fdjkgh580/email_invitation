68cb72b50aac34bcb7eeb744381200af3a71c461
jsn 筆電 <fdjkgh580@yahoo.com.tw>
2015-03-23 17:41:27 +0800

    修正註解與初始化

--- appscript/libraries/screen_do.js
+++ appscript/libraries/screen_do.js
@@ -1,19 +1,40 @@
-//屏幕判別後做什麼事情...
+//監控屏幕，判別當前解析度後做什麼事情...
 var screen_do = new function (){
+
+    //判別的選擇器
+    this.selector_screen = ".current_screen";
 
     /**
      * 屏幕判別
      * @return  "mobile" | "pad" | "desktop"
      */
     this.current = function (){
-        var scr = $(".current_screen").find("li:visible").attr("class");
+        var scr = $(this.selector_screen).find("li:visible").attr("class");
         if (scr == "mobile") return scr;
         else if (scr == "pad") return scr;
         else if (scr == "desktop") return scr;
-        else console.log("找不到偵測標籤 .current_screen");
+        else return false;
     }
 
+    this.init = function (){
+        var num = $(this.selector_screen).length;
+
+        if (num == 0) {
+            console.log("識別偵測版面的元素不存在，請先建立。");
+        }
+    }
+
+
+    /**
+     * 監控當視窗發生改變時，會執行什麼事情
+     * @param   function param.mobile   手機   
+     * @param   function param.pad      平板
+     * @param   function param.desktop  桌電 
+     */
     this.resize = function (param){
+
+        this.init();
+
         if (this.current() == "mobile") {
             if (param.mobile) param.mobile();
         }
