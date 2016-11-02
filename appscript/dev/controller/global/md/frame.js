f2649408a216ebe0f8a984ffecdc55f03de716ed
jsn 筆電 <fdjkgh580@yahoo.com.tw>
2016-11-02 12:13:21 +0800

    修正 vmodel 升級後的寫法

--- appscript/dev/controller/global/md/frame.js
+++ appscript/dev/controller/global/md/frame.js
@@ -1,37 +1,44 @@
 $(function (){
-    $(window).vmodel("--global/frame", true, function (){
-        var vs = this;
-        this.autoload = ['doevent', 'screen_do'];
 
-        // 依照不同尺寸螢幕
-        this.screen_do = function (){
-            $(window).resize(function (){
-                vs.doevent();
-            });
-        }
+    $.vmodel.create({
+        selector: window,
+        model: '--global/frame',
+        isautoload: true,
+        method: function (){
+            var vs = this;
+            this.autoload = ['doevent', 'screen_do'];
+            
+            // 依照不同尺寸螢幕
+            this.screen_do = function (){
+                $(window).resize(function (){
+                    vs.doevent();
+                });
+            }
 
-        this.doevent = function(){
-            $.vmodel.get("global/diff_screen").event({
-                
-                //手機
-                mobile: function (){require(
-                [
-                    'mobile'
-                ])},
+            this.doevent = function(){
+                $.vmodel.get("global/diff_screen").event({
+                    
+                    //手機
+                    mobile: function (){require(
+                    [
+                        'mobile'
+                    ])},
 
-                //平板
-                pad: function (){require(
-                [
-                    'pad'
-                ])},
-                
-                //桌電
-                desktop: function (){require(
-                [
-                    'desktop',
-                ])}
-            });
+                    //平板
+                    pad: function (){require(
+                    [
+                        'pad'
+                    ])},
+                    
+                    //桌電
+                    desktop: function (){require(
+                    [
+                        'desktop',
+                    ])}
+                });
+            }
         }
     });
+    
     
 })
